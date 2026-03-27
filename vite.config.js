6781c12275ed2c406b7dc48aa3eceec3c988e98b
saadsayed15 <saadsayed667@gmail.com>
2026-03-27 18:20:57 +0200

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/',
+  base: "/",
   server: {
     proxy: {
       "/api": {
